f16dafd3732bec9674ca71cf9fc3d6a719d382d0
Ben Gullette <jb@topapplabs.com>
2020-11-22 20:27:47 -0500

    Initial checkin

---
+++ SimpleVideo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SimpleVideo
+//
+//  Created by Ben on 11/22/20.
+//  Copyright © 2020 Ben. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playVideo(_ sender: UIButton) {
+        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+        return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
+}
+
